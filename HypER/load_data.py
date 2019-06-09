2617f62edaee56bebd1322e30d5258708837ffde
Chief <luyolo.nqobile@gmail.com>
2019-06-09 09:19:22 +0200

    HypER Model
    - Logits model output
    - Batch normalisation
    - requirements.txt: application dependencies
    - .gitignore

--- HypER/load_data.py
+++ HypER/load_data.py
@@ -1,20 +1,23 @@
 class Data:
 
     def __init__(self, data_dir="data/FB15k-237", reverse=False):
+
         self.train_data = self.load_data(data_dir, "train", reverse=reverse)
         self.valid_data = self.load_data(data_dir, "valid", reverse=reverse)
         self.test_data = self.load_data(data_dir, "test", reverse=reverse)
+
         self.data = self.train_data + self.valid_data + self.test_data
         self.entities = self.get_entities(self.data)
         self.train_relations = self.get_relations(self.train_data)
         self.valid_relations = self.get_relations(self.valid_data)
         self.test_relations = self.get_relations(self.test_data)
-        self.relations = self.train_relations + [i for i in self.valid_relations \
-                if i not in self.train_relations] + [i for i in self.test_relations \
-                if i not in self.train_relations]
+
+        self.relations = self.train_relations + [i for i in self.valid_relations
+                                                 if i not in self.train_relations] + [i for i in self.test_relations
+                                                                                      if i not in self.train_relations]
 
     def load_data(self, data_dir, data_type="train", reverse=False):
-        with open("%s%s.txt" % (data_dir, data_type), "r") as f:
+        with open("/Users/luyolomagangane/workspace/HypER/HypER/%s%s.txt" % (data_dir, data_type), "r") as f:
             data = f.read().strip().split("\n")
             data = [i.split() for i in data]
             if reverse:
